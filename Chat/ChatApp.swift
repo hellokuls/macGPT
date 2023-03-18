2dfcf7e221801e1b0ad4a5c5df6269e6cf0e28aa
liushuang <liushuang@laiye.com>
2023-03-18 17:04:52 +0800

    优化新增会话界面，之前的太丑了

--- Chat/ChatApp.swift
+++ Chat/ChatApp.swift
@@ -17,7 +17,4 @@ struct ChatApp: App {
         }
         
     }
-    
-    
-   
 }
--- Chat/ChatView/ChatView.swift
+++ Chat/ChatView/ChatView.swift
@@ -38,9 +38,12 @@ struct ChatView: View {
                                             DispatchQueue.main.async {
                                                 self.sessionsModel.chatViewModels[sessionInfo.id]?.deleteSessionInfo(sessionId: sessionInfo.id)
                                                 self.sessionsModel.chatViewModels.removeValue(forKey: sessionInfo.id)
-                                                if select == sessionsModel.sessionInfoList[self.sessionsModel.sessionInfoList.count - 1].name{
+                                                if select == sessionsModel.sessionInfoList[self.sessionsModel.sessionInfoList.count - 1].name {
                                                     self.sessionsModel.sessionInfoList.removeAll(where: {$0.id == sessionInfo.id})
                                                     select = sessionsModel.sessionInfoList[self.sessionsModel.sessionInfoList.count - 1].name
+                                                }else if(select == sessionsModel.sessionInfoList[0].name){
+                                                    self.sessionsModel.sessionInfoList.removeAll(where: {$0.id == sessionInfo.id})
+                                                    select = sessionsModel.sessionInfoList[0].name
                                                 }
                                             }
                                     }) {
@@ -53,19 +56,35 @@ struct ChatView: View {
                     Spacer()
                 }.listStyle(SidebarListStyle())
 
-                
                 // 添加一个按钮来切换列表的显示模式
                 VStack {
                     Button(action: {
-                        showAddSession.toggle()
+                        let alert = NSAlert()
+                        alert.messageText = "新增会话"
+                        alert.informativeText = "请输入会话名称"
+                        alert.addButton(withTitle: "OK")
+                        alert.addButton(withTitle: "Cancel")
+                        let contentView = NSView(frame: NSRect(x: 0, y: 0, width: 200, height: 150))
+
+                        let textField1 = NSTextField(frame: NSRect(x: 0, y: 120, width: 200, height: 25))
+                        textField1.placeholderString = "会话名称"
+                        textField1.lineBreakMode = .byClipping
+                        contentView.addSubview(textField1)
+                        
+                        let textField2 = NSTextField(frame: NSRect(x: 0, y: 0, width: 200, height: 100))
+                        textField2.placeholderString = "高阶玩法：提示词...开发中"
+                        contentView.addSubview(textField2)
+
+                        alert.accessoryView = contentView
+                        let result = alert.runModal()
+                        if result == .alertFirstButtonReturn {
+                            self.sessionsModel.cacheAPIKey(apiKey: textField1.stringValue)
+                        }
                     }) {
                         Text("添加会话")
                             .foregroundColor(.white)
                             .padding()
-                    }.sheet(isPresented: $showAddSession) {
-                      EditView(sessionsModel1: sessionsModel, showAddSession: $showAddSession,select: $select)
-                     }
-                    
+                    }
                     Button(action: {
                         
                     }) {
@@ -73,7 +92,7 @@ struct ChatView: View {
                             .foregroundColor(.white)
                             .padding()
                     }
-                    Text("Version 1.0.0")
+                    Text("Version 1.0.2")
                 }.padding()
                 
                     
@@ -81,7 +100,20 @@ struct ChatView: View {
             
         }.toolbar{
             Button(action: {
-                showAlert.toggle()
+                let alert = NSAlert()
+                
+                alert.informativeText = "请输你的api_key"
+                alert.addButton(withTitle: "OK")
+                alert.addButton(withTitle: "Cancel")
+                
+                let textField = NSSecureTextField(frame: NSRect(x: 0, y: 0, width: 200, height: 24))
+                textField.lineBreakMode = .byClipping
+                alert.accessoryView = textField
+                
+                let result = alert.runModal()
+                if result == .alertFirstButtonReturn {
+                    self.sessionsModel.cacheAPIKey(apiKey: textField.stringValue)
+                }
             })
             {
                 Image(systemName: "plus")
@@ -92,25 +124,6 @@ struct ChatView: View {
             AlertToast(displayMode: .hud, type: .error(Color.red), title: viewModel.chatErr.message)
        }.onAppear {
            bindToast()
-        }.alert(isPresented: $showAlert){
-            let alert = NSAlert()
-            alert.messageText = "MacGPT"
-            alert.informativeText = "请输你的api_key"
-            alert.addButton(withTitle: "OK")
-            alert.addButton(withTitle: "Cancel")
-
-            let textField = NSTextField(frame: NSRect(x: 0, y: 0, width: 200, height: 24))
-            textField.lineBreakMode = .byClipping
-            alert.accessoryView = textField
-
-            let result = alert.runModal()
-            if result == .alertFirstButtonReturn {
-                self.sessionsModel.cacheAPIKey(apiKey: textField.stringValue)
-                return Alert(title: Text("更新api_key成功！"))
-            }else{
-                return Alert(title: Text("取消成功"))
-            }
-            
         }
     }
     
@@ -132,27 +145,48 @@ struct EditView: View {
     @Binding var showAddSession : Bool
     @State private var sessionName = ""
     @Binding var select: String?
+    @State private var articleText = ""
     var body: some View {
         VStack {
-            Image("AppIcon")
+//            Image("128.png")
+
+
+//
+//            TextEditor(text: $articleText)
+//                            .frame(minHeight: 100)
+//                            .cornerRadius(5)
+//                            .padding()
+//                            .overlay(
+//                                RoundedRectangle(cornerRadius: 5)
+//                                    .stroke(Color.gray)
+//                            )
+//                            .padding().background(Color(NSColor.windowBackgroundColor).opacity(0.4))
+            Text("新建会话")
+                .font(.headline)
+                .padding(.bottom, 10)
+            Text("Popup Message")
+                .font(.subheadline)
+                .multilineTextAlignment(.center)
+                .padding(.bottom, 20)
+
             TextField("输入会话名称", text: $sessionName)
                         .padding()
                         .cornerRadius(10)
-                        .font(.system(size: 18))
+                        .font(.system(size: 15)).background(Color(NSColor.windowBackgroundColor).opacity(0.4))
             HStack {
                 Button(action: {
                     showAddSession = false
                 }) {
                     Text("取消")
                         .foregroundColor(.white)
-                        .padding()
+                        .padding(10)
                         .background(Color.red)
                         .cornerRadius(10)
                 }
                 .buttonStyle(PlainButtonStyle())
-                
+
                 Spacer()
-                
+
                 Button(action: {
                     // 在这里执行创建会话的操作
                     let id = viewModel.insertSessionInfo(name: sessionName)
@@ -162,7 +196,7 @@ struct EditView: View {
                 }) {
                     Text("创建")
                         .foregroundColor(.white)
-                        .padding()
+                        .padding(10)
                         .background(Color.green)
                         .cornerRadius(10)
                 }
@@ -170,7 +204,7 @@ struct EditView: View {
             }
             .padding(.horizontal)
         }
-        .frame(width: 300, height: 200)
+        .frame(width: 300, height: 300).padding().background(Color(NSColor.windowBackgroundColor)).opacity(0.8)
     }
 }
 
